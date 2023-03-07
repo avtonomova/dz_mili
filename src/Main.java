7833190713a7a17f0218e240ed2daf570da2df0b
Avtonomova <niva1090@mail.ru>
2023-03-07 14:55:52 +1000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 2000;
+        int mile = 1;
+
+        int bonus = price / 20 * mile;
+
+
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
